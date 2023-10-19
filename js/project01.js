b727e9ed4dcb92982a9e308f5e5325914ea0ed39
Chaeyun-Lee <145410502+Chaeyun-Lee@users.noreply.github.com>
2023-10-19 18:10:16 +0900

    Add files via upload

--- js/project01.js
+++ js/project01.js
@@ -1,37 +1,37 @@
 //Modal
-const modal = document.querySelector('#adiClub');
+const modal = document.querySelector("#adiClub");
 
 modal.showModal();
-window.addEventListener('scroll',()=>{
-  
-  const topMenu = document.querySelector('#headerBottom')
-  if(window.scrollY>10){
-    topMenu.style.position='fixed';
-    topMenu.style.top='0'
-    topMenu.style.backgroundColor='#ffffff'
-  } else {topMenu.style.position='relative';
-
+window.addEventListener("scroll", () => {
+  const topMenu = document.querySelector("#headerBottom");
+  if (window.scrollY > 10) {
+    topMenu.style.position = "fixed";
+    topMenu.style.top = "0";
+    topMenu.style.zIndex = "50";
+    topMenu.style.backgroundColor = "#ffffff";
+  } else {
+    topMenu.style.position = "relative";
   }
-})
-//Menu 
+});
+//Menu
 const mainList = document.querySelectorAll(".mainList");
 const dropMenu = document.querySelectorAll(".dropdownMenu");
 
-//Subli 
+//Subli
 const menSubList = document.querySelectorAll(".menSubli");
 const womenSubList = document.querySelectorAll(".womenSubli");
 const kidsSubList = document.querySelectorAll(".kidsSubli");
 const sportsSubList = document.querySelectorAll(".sportsSubli");
 const brandsSubList = document.querySelectorAll(".brandsSubli");
 
-//RightList 
+//RightList
 const menRightSubList = document.querySelectorAll(".menRightList");
 const womenRightSubList = document.querySelectorAll(".womenRightList");
 const kidsRightSubList = document.querySelectorAll(".kidsRightList");
 const sportsRightSubList = document.querySelectorAll(".sportsRightList");
 const brandsRightSubList = document.querySelectorAll(".brandsRightList");
 
-//toggle용 
+//toggle용
 const menRightBestLi = document.querySelectorAll(".menSubli #rightBest>li");
 const menRightNewLi = document.querySelectorAll(".menSubli #rightNew>li");
 const womenRightBestLi = document.querySelectorAll(".womenSubli #rightBest>li");
@@ -40,102 +40,102 @@ const kidsRightBestLi = document.querySelectorAll(".kidsSubli #rightBest>li");
 const kidsRightNewLi = document.querySelectorAll(".kidsSubli #rightNew>li");
 
 //Men 베스트셀러, 신상 카테고리 Hover 시 Toggle 이벤트
-for(let i=0; i<menRightBestLi.length;i++){
-  let toggleTextBox  = menRightBestLi[i].querySelector('.toggleTextBox');
-  let rightImgBox  = menRightBestLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < menRightBestLi.length; i++) {
+  let toggleTextBox = menRightBestLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = menRightBestLi[i].querySelector(".rightImgBox");
 
-  menRightBestLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)'
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  menRightBestLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)'
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  menRightBestLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  menRightBestLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
-for(let i=0; i<menRightNewLi.length;i++){
-  let toggleTextBox = menRightNewLi[i].querySelector('.toggleTextBox');
-  let rightImgBox = menRightNewLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < menRightNewLi.length; i++) {
+  let toggleTextBox = menRightNewLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = menRightNewLi[i].querySelector(".rightImgBox");
 
-  menRightNewLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)';
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  menRightNewLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)';
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  menRightNewLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  menRightNewLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
 //Women 베스트셀러, 신상 카테고리 Hover 시 Toggle 이벤트
-for(let i=0; i<womenRightBestLi.length;i++){
-  let toggleTextBox  = womenRightBestLi[i].querySelector('.toggleTextBox');
-  let rightImgBox  = womenRightBestLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < womenRightBestLi.length; i++) {
+  let toggleTextBox = womenRightBestLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = womenRightBestLi[i].querySelector(".rightImgBox");
 
-  womenRightBestLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)'
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  womenRightBestLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)'
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  womenRightBestLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  womenRightBestLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
-for(let i=0; i<womenRightNewLi.length;i++){
-  let toggleTextBox = womenRightNewLi[i].querySelector('.toggleTextBox');
-  let rightImgBox = womenRightNewLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < womenRightNewLi.length; i++) {
+  let toggleTextBox = womenRightNewLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = womenRightNewLi[i].querySelector(".rightImgBox");
 
-  womenRightNewLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)';
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  womenRightNewLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)';
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  womenRightNewLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  womenRightNewLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
 //Kids 베스트셀러, 신상 카테고리 Hover 시 Toggle 이벤트
-for(let i=0; i<kidsRightBestLi.length;i++){
-  let toggleTextBox  = kidsRightBestLi[i].querySelector('.toggleTextBox');
-  let rightImgBox  = kidsRightBestLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < kidsRightBestLi.length; i++) {
+  let toggleTextBox = kidsRightBestLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = kidsRightBestLi[i].querySelector(".rightImgBox");
 
-  kidsRightBestLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)'
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  kidsRightBestLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)'
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  kidsRightBestLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  kidsRightBestLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
-for(let i=0; i<kidsRightNewLi.length;i++){
-  let toggleTextBox = kidsRightNewLi[i].querySelector('.toggleTextBox');
-  let rightImgBox = kidsRightNewLi[i].querySelector('.rightImgBox');
+for (let i = 0; i < kidsRightNewLi.length; i++) {
+  let toggleTextBox = kidsRightNewLi[i].querySelector(".toggleTextBox");
+  let rightImgBox = kidsRightNewLi[i].querySelector(".rightImgBox");
 
-  kidsRightNewLi[i].addEventListener('mouseover',()=>{
-    toggleTextBox.style.transform='translateY(-50%)';
-    toggleTextBox.style.opacity='1';
-    rightImgBox.children[1].style.visibility='visible';
-  })
-  kidsRightNewLi[i].addEventListener('mouseout',()=>{
-    toggleTextBox.style.transform='translateY(0%)';
-    toggleTextBox.style.opacity='0';
-    rightImgBox.children[1].style.visibility='hidden';
-  })
+  kidsRightNewLi[i].addEventListener("mouseover", () => {
+    toggleTextBox.style.transform = "translateY(-50%)";
+    toggleTextBox.style.opacity = "1";
+    rightImgBox.children[1].style.visibility = "visible";
+  });
+  kidsRightNewLi[i].addEventListener("mouseout", () => {
+    toggleTextBox.style.transform = "translateY(0%)";
+    toggleTextBox.style.opacity = "0";
+    rightImgBox.children[1].style.visibility = "hidden";
+  });
 }
 
 //드롭다운 메뉴 제어, 이 코드 줄이는 방법!알아오기
@@ -189,15 +189,59 @@ for (let i = 0; i < sportsSubList.length; i++) {
   });
 }
 for (let i = 0; i < brandsSubList.length; i++) {
-    brandsSubList[i].addEventListener("mouseover", () => {
-      brandsRightSubList[0].style.display = "none";
-      brandsRightSubList[i].style.display = "flex";
-    });
-    brandsSubList[i].addEventListener("mouseout", () => {
-      brandsRightSubList[i].style.display = "none";
-      brandsRightSubList[0].style.display = "flex";
-    });
+  brandsSubList[i].addEventListener("mouseover", () => {
+    brandsRightSubList[0].style.display = "none";
+    brandsRightSubList[i].style.display = "flex";
+  });
+  brandsSubList[i].addEventListener("mouseout", () => {
+    brandsRightSubList[i].style.display = "none";
+    brandsRightSubList[0].style.display = "flex";
+  });
+}
+
+//드롭다운 script 끝//
+
+// terrex 캐러셀 script
+let myIndex = 0;
+const itemScreen = document.querySelector("#itemScreen");
+carousel();
+setInterval(carousel, 3000);
+
+function carousel() {
+  if (myIndex < 600) {
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+    myIndex += 600;
+  } else if (myIndex >= 600 && myIndex <= 1200) {
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+    myIndex += 600;
+  } else if (myIndex >= 1200) {
+    myIndex = 0;
+    itemScreen.style.transform = `translateX(${myIndex}px)`;
   }
+}
 
+const leftB = document.querySelector("#leftBtn");
+const rightB = document.querySelector("#rightBtn");
 
-  //드롭다운 script 끝//
+leftB.addEventListener("click", leftBEvent);
+rightB.addEventListener("click", rightBEvent);
+
+function leftBEvent() {
+  if (myIndex > 0) {
+    myIndex -= 600;
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+  } else if (myIndex == 0) {
+    myIndex = 1200;
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+  }
+}
+
+function rightBEvent() {
+  if (myIndex >= 0 && myIndex < 1200) {
+    myIndex += 600;
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+  } else if (myIndex >= 1200) {
+    myIndex = 0;
+    itemScreen.style.transform = `translateX(-${myIndex}px)`;
+  }
+}
